501da66243fef9c4e44306cd9142acd513fc9735
Maciek <mfrankowski@fortech.krakow.pl>
2019-03-28 12:31:37 +0100

    Moolog bundle has been installed.

--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -5,6 +5,7 @@ namespace App\Security;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Exception\InvalidConfirmationTokenException;
+use Psr\Log\LoggerInterface;
 
 
 class UserConfirmationService
@@ -19,26 +20,37 @@ class UserConfirmationService
      */
     private $entityManager;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         UserRepository $userRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger
     ) {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken) 
     {
+        $this->logger->debug('Fetching user by confirmation token');
         $user = $this->userRepository->findOneBy(
             ['confirmationToken' => $confirmationToken]
         );
 
         if (!$user) {
+            $this->logger->debug('User by confirmation token not found');
             throw new InvalidConfirmationTokenException();
         }
         
         $user->setEnabled(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token');
     }
 }
