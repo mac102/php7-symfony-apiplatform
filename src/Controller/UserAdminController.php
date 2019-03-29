ac5953937c9781db8bfed4a68b12643ba9a492ac
Maciek <mfrankowski@fortech.krakow.pl>
2019-03-29 13:44:18 +0100

    Easy Admin config update.

---
+++ src/Controller/UserAdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserAdminController extends BaseAdminController
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    protected function persistEntity($entity) 
+    {
+        $this->encodeUserPassword($entity);
+        parent::persistEntity($entity);
+    }
+
+    protected function updateEntity($entity)
+    {
+        $this->encodeUserPassword($entity);
+        parent::updateEntity($entity);
+    }
+
+    private function encodeUserPassword($entity) : void
+    {
+        $entity->setPassword(
+            $this->passwordEncoder->encodePassword($entity, $entity->getPassword())
+        );
+    }
+}
