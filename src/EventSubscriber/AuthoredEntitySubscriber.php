5dca15662b9b1b1bfea6ad5f6d5782c5478c1c40
Maciek <mfrankowski@fortech.krakow.pl>
2019-04-02 13:35:13 +0200

    Token null  validation has been added.

--- src/EventSubscriber/AuthoredEntitySubscriber.php
+++ src/EventSubscriber/AuthoredEntitySubscriber.php
@@ -2,15 +2,14 @@
 
 namespace App\EventSubscriber;
 
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\KernelEvents;
 use ApiPlatform\Core\EventListener\EventPriorities;
-use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use App\Entity\BlogPost;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Comment;
 use App\Entity\AuthoredEntityInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class AuthoredEntitySubscriber implements EventSubscriberInterface
 {
@@ -33,7 +32,12 @@ class AuthoredEntitySubscriber implements EventSubscriberInterface
         $entity = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        $author = $this->tokenStorage->getToken()->getUser();
+        $token = $this->tokenStorage->getToken();
+        if (null === $token) {
+            return;
+        }
+
+        $author = $token->getUser();
 
         if (!$entity instanceof AuthoredEntityInterface || Request::METHOD_POST !== $method) {
             return;
