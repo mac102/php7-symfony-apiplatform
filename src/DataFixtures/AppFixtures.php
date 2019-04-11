0b3846fa885a7a481c12c131ebfd910e4b180408
Maciek <mfrankowski@fortech.krakow.pl>
2019-04-11 14:01:15 +0200

    Extend Fixtures class.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,88 +2,179 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
+use App\Entity\Comment;
+use App\Entity\User;
+use App\Security\TokenGenerator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use App\Entity\BlogPost;
-use App\Entity\User;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use App\Entity\Comment;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
     private $passwordEncoder;
-
     /**
      * @var \Faker\Factory
      */
     private $faker;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    private const USERS = [
+        [
+            'username' => 'maciek',
+            'email' => 'admin@blog.com',
+            'name' => 'Mac',
+            'password' => 'Maciek12345',
+            'roles' => [User::ROLE_SUPERADMIN],
+            'enabled' => true
+        ],
+        [
+            'username' => 'john_doe',
+            'email' => 'john@blog.com',
+            'name' => 'John Doe',
+            'password' => 'secret123#',
+            'roles' => [User::ROLE_ADMIN],
+            'enabled' => true
+        ],
+        [
+            'username' => 'rob_smith',
+            'email' => 'rob@blog.com',
+            'name' => 'Rob Smith',
+            'password' => 'secret123#',
+            'roles' => [User::ROLE_WRITER],
+            'enabled' => true
+        ],
+        [
+            'username' => 'jenny_rowling',
+            'email' => 'jenny@blog.com',
+            'name' => 'Jenny Rowling',
+            'password' => 'secret123#',
+            'roles' => [User::ROLE_WRITER],
+            'enabled' => true
+        ],
+        [
+            'username' => 'han_solo',
+            'email' => 'han@blog.com',
+            'name' => 'Han Solo',
+            'password' => 'secret123#',
+            'roles' => [User::ROLE_EDITOR],
+            'enabled' => false
+        ],
+        [
+            'username' => 'jedi_knight',
+            'email' => 'jedi@blog.com',
+            'name' => 'Jedi Knight',
+            'password' => 'secret123#',
+            'roles' => [User::ROLE_COMMENTATOR],
+            'enabled' => true
+        ],
+    ];
+    /**
+     * @var TokenGenerator
+     */
+    private $tokenGenerator;
+    public function __construct(
+        UserPasswordEncoderInterface $passwordEncoder,
+        TokenGenerator $tokenGenerator
+    )
     {
         $this->passwordEncoder = $passwordEncoder;
         $this->faker = \Faker\Factory::create();
+        $this->tokenGenerator = $tokenGenerator;
     }
-
+    /**
+     * Load data fixtures with the passed EntityManager
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager)
     {
         $this->loadUsers($manager);
         $this->loadBlogPosts($manager);
         $this->loadComments($manager);
     }
-
     public function loadBlogPosts(ObjectManager $manager)
     {
-        $user = $this->getReference('user_admin');
-
-        for ($i=0; $i< 100; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $blogPost = new BlogPost();
             $blogPost->setTitle($this->faker->realText(30));
-            $blogPost->setPublished($this->faker->datetimeThisYear);
+            $blogPost->setPublished($this->faker->dateTimeThisYear);
             $blogPost->setContent($this->faker->realText());
-            $blogPost->setAuthor($user);
+            $authorReference = $this->getRandomUserReference($blogPost);
+            $blogPost->setAuthor($authorReference);
             $blogPost->setSlug($this->faker->slug);
-            
             $this->setReference("blog_post_$i", $blogPost);
-
             $manager->persist($blogPost);
         }
         $manager->flush();
     }
-
     public function loadComments(ObjectManager $manager)
     {
-        $user = $this->getReference('user_admin');
-
-        for ($i=0; $i< 100; $i++) {
-            for ($j=0; $j< rand(1,10); $j++) {
+        for ($i = 0; $i < 100; $i++) {
+            for ($j = 0; $j < rand(1, 10); $j++) {
                 $comment = new Comment();
                 $comment->setContent($this->faker->realText());
-                $comment->setPublished($this->faker->datetimeThisYear);
-                $comment->setAuthor($user);
+                $comment->setPublished($this->faker->dateTimeThisYear);
+                $authorReference = $this->getRandomUserReference($comment);
+                $comment->setAuthor($authorReference);
                 $comment->setBlogPost($this->getReference("blog_post_$i"));
                 $manager->persist($comment);
             }
         }
         $manager->flush();
     }
-
     public function loadUsers(ObjectManager $manager)
     {
-        $user = new User();
-        $user->setUsername('admin');
-        $user->setEmail('admin@blog.pl');
-        $user->setName('Maciek');
-        $user->setEnabled(true);
-        $user->setPassword(
-            $this->passwordEncoder->encodePassword(
-                $user,
-                'mac123'
-            )
-        );
-
-        $this->addReference('user_admin', $user);
-
-        $manager->persist($user);
+        foreach (self::USERS as $userFixture) {
+            $user = new User();
+            $user->setUsername($userFixture['username']);
+            $user->setEmail($userFixture['email']);
+            $user->setName($userFixture['name']);
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword(
+                    $user,
+                    $userFixture['password']
+                )
+            );
+            $user->setRoles($userFixture['roles']);
+            $user->setEnabled($userFixture['enabled']);
+            if (!$userFixture['enabled']) {
+                $user->setConfirmationToken(
+                    $this->tokenGenerator->getRandomSecureToken()
+                );
+            }
+            $this->addReference('user_'.$userFixture['username'], $user);
+            $manager->persist($user);
+        }
         $manager->flush();
+    }
+    protected function getRandomUserReference($entity): User
+    {
+        $randomUser = self::USERS[rand(0, 5)];
+        if ($entity instanceof BlogPost && !count(
+                array_intersect(
+                    $randomUser['roles'],
+                    [User::ROLE_SUPERADMIN, User::ROLE_ADMIN, User::ROLE_WRITER]
+                )
+            )) {
+            return $this->getRandomUserReference($entity);
+        }
+        if ($entity instanceof Comment && !count(
+                array_intersect(
+                    $randomUser['roles'],
+                    [
+                        User::ROLE_SUPERADMIN,
+                        User::ROLE_ADMIN,
+                        User::ROLE_WRITER,
+                        User::ROLE_COMMENTATOR,
+                    ]
+                )
+            )) {
+            return $this->getRandomUserReference($entity);
+        }
+        return $this->getReference(
+            'user_'.$randomUser['username']
+        );
     }
 }
